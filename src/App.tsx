76a57c77d775f38bcbb11e0e78f09a8d6cf2bb83
adrimaximus <adri@7inked.com>
2025-09-26 18:00:43 +0700

    Init Dyad app

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LaundryDashboard from "./pages/LaundryDashboard";
+import OrdersPage from "./pages/OrdersPage"; // Import OrdersPage
+import SettingsPage from "./pages/SettingsPage"; // Route for Settings page
+import NewOrderPage from "./pages/NewOrderPage"; // Route for creating a new order
+import "./globals.css"; // Mengubah impor dari index.css ke globals.css
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LaundryDashboard />} />
+        <Route path="/orders/new" element={<NewOrderPage />} /> {/* Rute untuk membuat pesanan baru */}
+        <Route path="/orders" element={<OrdersPage />} /> {/* Rute baru untuk halaman pesanan */}
+        <Route path="/settings" element={<SettingsPage />} /> {/* Rute untuk halaman pengaturan */}
+        {/* Tambahkan rute lain di sini jika diperlukan */}
+      </Routes>
+    </Router>);
+
+}
+
+export default App;
